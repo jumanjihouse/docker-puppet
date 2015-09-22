0258e968b9df687651bd9e89fe38aa8fb01a16ba
Paul Morgan <jumanjiman@gmail.com>
2015-09-22 11:13:04 -0400

    puppetboard: install from master branch instead of PyPI
    PyPI has an old version of the package.
    We would like to use features that have been merged into master,
    such as the catalog viewer.

--- puppetboard/files/var/www/puppetboard/settings.py
+++ puppetboard/files/var/www/puppetboard/settings.py
@@ -1,3 +1,5 @@
+import os
+
 DEV_LISTEN_HOST = '127.0.0.1'
 DEV_LISTEN_PORT = 9090
 ENABLE_QUERY = True
@@ -10,6 +12,10 @@ PUPPETDB_CERT = '/var/lib/puppet/ssl/certs/puppetboard.inf.ise.com.pem'
 PUPPETDB_KEY = '/var/lib/puppet/ssl/private_keys/puppetboard.inf.ise.com.pem'
 PUPPETDB_SSL_VERIFY = False
 PUPPETDB_TIMEOUT = 20
+SECRET_KEY = os.urandom(24)
+DEV_COFFEE_LOCATION = 'coffee'
+OFFLINE_MODE = False
+ENABLE_CATALOG = False
 REPORTS_COUNT = 40
 UNRESPONSIVE_HOURS = 25
 GRAPH_FACTS = ['architecture',
@@ -23,3 +29,9 @@ GRAPH_FACTS = ['architecture',
                'osfamily',
                'puppetversion',
                'processorcount']
+INVENTORY_FACTS = [ ('Hostname',       'fqdn'              ),
+                    ('IP Address',     'ipaddress'         ),
+                    ('OS',             'lsbdistdescription'),
+                    ('Architecture',   'hardwaremodel'     ),
+                    ('Kernel Version', 'kernelrelease'     ),
+                    ('Puppet Version', 'puppetversion'     ), ]
--- script/15_build_puppetboard.sh
+++ script/15_build_puppetboard.sh
@@ -10,7 +10,24 @@ base_tag="jumanjiman/puppetboard"
 hash_tag="${base_tag}:${short_hash}"
 late_tag="${base_tag}:latest"
 pushd puppetboard
+(
 cp -r ../ssl .
-smitty docker build --rm -t $hash_tag . 2>&1 | tee /tmp/build-puppetboard.out
+
+# Build an image that creates a pip package.
+smitty docker build --rm -t puppetboard:build -f Dockerfile.build .
+
+# Create a container from which to copy files.
+docker rm -f puppetboard_build || :
+smitty docker create --name puppetboard_build puppetboard:build true
+
+# Grab the pip tarball.
+smitty docker cp puppetboard_build:/puppetboard/dist/puppetboard-master.tar.gz files/
+
+# Grab a copy of default_settings.py from master branch for local review if necessary.
+smitty docker cp puppetboard_build:/puppetboard/puppetboard/default_settings.py files/var/www/puppetboard/
+
+# Build the runtime image.
+smitty docker build --rm -t $hash_tag -f Dockerfile .
+) 2>&1 | tee /tmp/build-puppetboard.out
 smitty docker tag -f $hash_tag $late_tag
 popd
