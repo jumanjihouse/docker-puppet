2d3466ba1b9d3b95917de32ae321d54f470e45cd
Paul Morgan <jumanjiman@gmail.com>
2017-07-05 20:53:08 +0000

    ci: deprecate our dns container in favor of docker dns
    This commit removes our bind (dns) container and
    is part of a series of commits to
    migrate the test harness to docker-compose.

--- script/05_build_named.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-set -o noclobber
-
-# Import smitty.
-source script/functions
-
-say Build dns server image.
-smitty docker build --rm -t jumanjiman/named named/ 2>&1 | tee /tmp/build-named.out
